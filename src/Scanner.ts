64222d95f403353ac5363b1d5a62ad9b6ba07338
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-04 11:48:03 +0100

    Implement scanner

---
+++ src/Scanner.ts
@@ -0,0 +1,185 @@
+import { Lox } from "./main";
+import { Token, TokenType } from "./TokenType";
+
+export class Scanner {
+  static keywords: { [key: string]: TokenType } = {
+    and: "AND",
+    class: "CLASS",
+    else: "ELSE",
+    false: "FALSE",
+    for: "FOR",
+    fun: "FUN",
+    if: "IF",
+    nil: "NIL",
+    or: "OR",
+    print: "PRINT",
+    return: "RETURN",
+    super: "SUPER",
+    this: "THIS",
+    true: "TRUE",
+    var: "VAR",
+    while: "WHILE",
+  };
+
+  private readonly tokens: Token[] = [];
+  private start = 0;
+  private current = 0;
+  private line = 1;
+
+  constructor(private readonly source: string) {}
+
+  scanTokens(): Token[] {
+    while (!this.isAtEnd()) {
+      this.start = this.current;
+      this.scanToken();
+    }
+
+    this.tokens.push(new Token("EOF", "", null, this.line));
+    return this.tokens;
+  }
+  isAtEnd(): boolean {
+    return this.current >= this.source.length;
+  }
+  scanToken(): void {
+    const c = this.advance();
+    switch (c) {
+      case "(":
+        this.addToken("LEFT_PAREN");
+        break;
+      case ")":
+        this.addToken("RIGHT_PAREN");
+        break;
+      case "{":
+        this.addToken("LEFT_BRACE");
+        break;
+      case "}":
+        this.addToken("RIGHT_BRACE");
+        break;
+      case ",":
+        this.addToken("COMMA");
+        break;
+      case ".":
+        this.addToken("DOT");
+        break;
+      case "-":
+        this.addToken("MINUS");
+        break;
+      case "+":
+        this.addToken("PLUS");
+        break;
+      case ";":
+        this.addToken("SEMICOLON");
+        break;
+      case "*":
+        this.addToken("STAR");
+        break;
+      case "!":
+        this.addToken(this.match("=") ? "BANG_EQUAL" : "BANG");
+        break;
+      case "=":
+        this.addToken(this.match("=") ? "EQUAL_EQUAL" : "EQUAL");
+        break;
+      case "<":
+        this.addToken(this.match("=") ? "LESS_EQUAL" : "LESS");
+        break;
+      case ">":
+        this.addToken(this.match("=") ? "GREATER_EQUAL" : "GREATER");
+        break;
+      case "/":
+        if (this.match("/")) {
+          while (this.peek() != "\n" && !this.isAtEnd()) this.advance();
+        } else {
+          this.addToken("SLASH");
+        }
+        break;
+      case " ":
+      case "\r":
+      case "\t":
+        break;
+      case "\n":
+        this.line++;
+        break;
+      case '"':
+        this.string();
+        break;
+      default:
+        if (this.isDigit(c)) {
+          this.number();
+        } else if (this.isAlpha(c)) {
+          this.identifier();
+        } else {
+          Lox.error(this.line, "Unexpected character");
+        }
+        break;
+    }
+  }
+  number(): void {
+    while (this.isDigit(this.peek())) this.advance();
+
+    if (this.peek() == "." && this.isDigit(this.peekNext())) {
+      this.advance();
+
+      while (this.isDigit(this.peek())) this.advance();
+    }
+
+    this.addToken(
+      "NUMBER",
+      Number.parseFloat(this.source.substring(this.start, this.current))
+    );
+  }
+  peekNext(): string {
+    if (this.current + 1 >= this.source.length) return "\0";
+    return this.source.charAt(this.current + 1);
+  }
+  identifier(): void {
+    while (this.isAlphaNumeric(this.peek())) this.advance();
+
+    const text = this.source.substring(this.start, this.current);
+    const type = Scanner.keywords[text] || "IDENTIFIER";
+
+    this.addToken(type);
+  }
+  isAlpha(c: string): boolean {
+    return (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") || c == "_";
+  }
+  isDigit(c: string): boolean {
+    return c >= "0" && c <= "9";
+  }
+  isAlphaNumeric(c: string): boolean {
+    return this.isAlpha(c) || this.isDigit(c);
+  }
+  string(): void {
+    while (this.peek() != '"' && !this.isAtEnd()) {
+      if (this.peek() == "\n") this.line++;
+      this.advance();
+    }
+
+    if (this.isAtEnd()) {
+      Lox.error(this.line, "Unterminated string.");
+      return;
+    }
+
+    this.advance();
+
+    const value = this.source.substring(this.start + 1, this.current - 1);
+    this.addToken("STRING", value);
+  }
+  peek(): string {
+    if (this.isAtEnd()) return "\0";
+    return this.source.charAt(this.current);
+  }
+  match(expected: string): boolean {
+    if (this.isAtEnd()) return false;
+    if (this.source.charAt(this.current) != expected) return false;
+
+    this.current++;
+    return true;
+  }
+  advance(): string {
+    return this.source.charAt(this.current++);
+  }
+  addToken(type: TokenType, literal: any = null): void {
+    const text = this.source.substring(this.start, this.current);
+    this.tokens.push(new Token(type, text, literal, this.line));
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,59 @@
-console.log("tslox!!");
+import { stdin as input, stdout as output, exit } from "node:process";
+import { readFileSync } from "node:fs";
+import * as readline from "node:readline/promises";
+import { Scanner } from "./Scanner";
 
-export {};
+export class Lox {
+  static error(line: number, message: string) {
+    Lox.report(line, "", message);
+  }
+
+  static hadError = false;
+
+  static report(line: number, where: string, message: string) {
+    console.error(`[line ${line}] Error${where}" ${message}`);
+    Lox.hadError = true;
+  }
+
+  static run(source: string) {
+    const scanner = new Scanner(source);
+    const tokens = scanner.scanTokens();
+
+    for (const token of tokens) {
+      console.log(token);
+    }
+  }
+
+  static runFile(path: string) {
+    Lox.run(readFileSync(path, { encoding: "utf-8" }));
+
+    if (Lox.hadError) {
+      exit(65);
+    }
+  }
+
+  static async runPrompt() {
+    const rl = readline.createInterface({ input, output, prompt: "> " });
+
+    rl.on("line", (line) => {
+      Lox.run(line);
+      Lox.hadError = false;
+      rl.prompt();
+    }).on("close", () => {
+      process.exit(0);
+    });
+  }
+
+  static main(args: string[]) {
+    if (args.length > 3) {
+      console.error("Usage: tslox [script]\n");
+      exit(64);
+    } else if (args.length == 1) {
+      Lox.runFile(args[0]);
+    } else {
+      Lox.runPrompt();
+    }
+  }
+}
+
+Lox.main(process.argv.slice(2));
