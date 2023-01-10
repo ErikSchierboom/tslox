0b7e3b4d298fe6be5d3ee30da09bd1ae070c8aaa
Erik Schierboom <erik_schierboom@hotmail.com>
2023-01-10 12:31:12 +0100

    Add arithmetic precendence tests

--- tests/interpreter.test.ts
+++ tests/interpreter.test.ts
@@ -1,8 +1,6 @@
 import { Lox } from "../src/Lox";
 
-const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
-const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
-
+beforeEach(() => jest.clearAllMocks());
 afterEach(() => jest.restoreAllMocks());
 
 test("arithmetic", () => {
@@ -19,7 +17,24 @@ test("arithmetic", () => {
   expect(logs).toEqual(expected);
 });
 
+test("arithmetic precedence", () => {
+  const source = `
+    print 2 - 1 + 3;
+    print 8 + 4 * 3;
+    print 3 * 4 / 2;
+    print 2 - (1 + 3);
+  `;
+
+  const { logs: logs } = run(source);
+
+  const expected = ["4", "20", "6", "-2"];
+  expect(logs).toEqual(expected);
+});
+
 function run(source: string): { logs: string[]; errors: string[] } {
+  const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
+  const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
+
   Lox.run(source);
 
   return {
